456dd46cd181b6c99bac6fb4ace285c434dec19d
pfmao <ErenJagerM@outlook.com>
2021-12-24 14:47:01 +0800

    Updates

---
+++ component---src-pages-404-js-8496be6da34716fa28df.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkattack_on_morty=self.webpackChunkattack_on_morty||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),o=a(7198),l=a(3751);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:u},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8496be6da34716fa28df.js.map
